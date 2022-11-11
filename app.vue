50b543f53b794351577457a59612d688852defab
tannal <tannal.cn@gmail.com>
2022-11-11 14:04:53 +0800

    add google site meta

--- app.vue
+++ app.vue
@@ -1,13 +1,13 @@
 <template>
   <Html :class="`${theme === 'dark' ? 'dark' : ''}`" :lang="locale">
-    <Body
-      class="antialiased duration-300 transition-colors text-gray-800 dark:text-gray-200 bg-white dark:bg-gray-900"
-    >
-      <NuxtLayout>
-        <NuxtLoadingIndicator :height="5" :duration="3000" :throttle="400" />
-        <NuxtPage />
-      </NuxtLayout>
-    </Body>
+
+  <Body class="antialiased duration-300 transition-colors text-gray-800 dark:text-gray-200 bg-white dark:bg-gray-900">
+    <NuxtLayout>
+      <NuxtLoadingIndicator :height="5" :duration="3000" :throttle="400" />
+      <NuxtPage />
+    </NuxtLayout>
+  </Body>
+
   </Html>
 </template>
 
@@ -29,6 +29,7 @@ useHead({
       name: 'description',
       content: 'Nuxt 3 Awesome Starter',
     },
+    { name: 'google-site-verification', content: 'QPn6QfdkYKKc1UsXKnuZ71-T7YUbwOVNYlMjmiD2VcM' },
   ],
   link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
 })
